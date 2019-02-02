00ebca2defbd0bceef9e80124745dc1d32f2f437
ayu69 <Q.marques58@gmail.com>
2019-02-02 11:59:00 +0100

    Add files via upload

--- RFID/lecture.py
+++ RFID/lecture.py
@@ -9,7 +9,11 @@ import MySQLdb
 import sys
 import os
 import time
+import xlrd
 
+out = open('/home/pi/RFID/lecture.log','w')
+sys.stdout = out
+sys.stderr = out
 
 # Variables for MySQL
 db = MySQLdb.connect(host="localhost", user="root",passwd="Melec", db="RFID")
@@ -19,10 +23,10 @@ continue_reading = True
 
 # Fonction qui arrete la lecture proprement
 def end_read(signal,frame):
-    global continue_reading
-    print ("Lecture terminée")
-    continue_reading = False
-    GPIO.cleanup()
+	global continue_reading
+	print ("Lecture terminée")
+	continue_reading = False
+	GPIO.cleanup()
 
 signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
@@ -32,49 +36,48 @@ while continue_reading:
 
 
 
-    os.system('sudo pkill -f /home/pi/RFID-RC522/MFRC522.py')
+	os.system('sudo pkill -f /home/pi/RFID-RC522/MFRC522.py')
 
-    # Detecter les tags
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+	# Detecter les tags
+	(status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # Une carte est detectee
-    if status == MIFAREReader.MI_OK:
-        print ("Carte detectee")
+	# Une carte est detectee
+	if status == MIFAREReader.MI_OK:
+		print ("Carte detectee")
 
-    # Recuperation UID
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+	# Recuperation UID
+	(status,uid) = MIFAREReader.MFRC522_Anticoll()
 
-    if status == MIFAREReader.MI_OK:
+	if status == MIFAREReader.MI_OK:
 
-        uid2 = (str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3]))
+		uid2 = (str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3]))
 
-        print uid2
-        
-        if uid2 is None:
-            GPIO.cleanup()
-        else:
-            print "Lecture de la base..."
+		print uid2
+		
+		try:
+			print "Lecture de la base..."
 
-            # Execute the SQL command
-            sql = "SELECT * FROM ELEVE WHERE UID = (%s)"
+			# Execute the SQL command
+			sql = "SELECT * FROM ELEVE WHERE UID = (%s)"
 
-            cur.execute(sql, [uid2])
+			cur.execute(sql, [uid2])
 
-            rows = cur.fetchall()
-     
-            print "ID = ",rows[0][0]
-            print "Nom = ",rows[0][1]
-            print "Prenom = ",rows[0][2]
-            print "Email = ",rows[0][3]
-            print "Groupe = ",rows[0][4]
-            print "Entreprise = ",rows[0][5]
-            print "UID = ",rows[0][6]
-            print "Heure = ",time.strftime("%Y-%m-%d %H:%M")
+			rows = cur.fetchall()
 
-        if status == MIFAREReader.MI_OK:
-            #MIFAREReader.MFRC522_Read(8)
-            MIFAREReader.MFRC522_StopCrypto1()
-            GPIO.cleanup()
+			print "ID : ",rows[0][0],"Nom : ",rows[0][1],"Prenom : ",rows[0][2],"Email : ",rows[0][3],"Groupe : ",rows[0][4],"Entreprise : ",rows[0][5],"Heure : ",time.strftime("%Y-%m-%d %H:%M")
+			#print "UID = ",rows[0][6]
 
-        else:
-            GPIO.cleanup()
+			#document = xlrd.open_workbook("Présence.xlsx")
+
+
+
+		except:
+			print("Carte Inconnu")
+
+
+		if status == MIFAREReader.MI_OK:
+			#MIFAREReader.MFRC522_Read(8)
+			MIFAREReader.MFRC522_StopCrypto1()
+
+		else:
+			print ("Erreur d\'Authentification")
