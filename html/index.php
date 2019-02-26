1e5fa52d750a7a995aa9bd2851b55ae25a25383a
ayu69 <Q.marques58@gmail.com>
2019-02-26 10:24:49 +0100

    Add files via upload

--- html/index.php
+++ html/index.php
@@ -79,23 +79,23 @@
                     <fieldset>
 
                         <!-- Form Name -->
-                        <legend>Form Name</legend>
+                        <legend>PointRFID</legend>
 
                         <!-- File Button -->
-                        <div class="form-group">
+                        <!--<div class="form-group">
                             <label class="col-md-4 control-label" for="filebutton">Select File</label>
                             <div class="col-md-4">
                                 <input type="file" name="file" id="file" class="input-large">
                             </div>
-                        </div>
+                        </div>-->
 
                         <!-- Button -->
-                        <div class="form-group">
+                        <!--<div class="form-group">
                             <label class="col-md-4 control-label" for="singlebutton">Import data</label>
                             <div class="col-md-4">
                                 <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Import</button>
                             </div>
-                        </div>
+                        </div>-->
 
                     </fieldset>
                 </form>
@@ -107,14 +107,14 @@
                get_all_records();
             ?>
                  <div>
-                            <form class="form-horizontal" action="functions.php" method="post" name="upload_excel"   
+                            <!-- <form class="form-horizontal" action="functions.php" method="post" name="upload_excel"   
                                       enctype="multipart/form-data">
                                   <div class="form-group">
                                             <div class="col-md-4 col-md-offset-4">
                                                 <input type="submit" name="Export" class="btn btn-success" value="export to excel"/>
                                             </div>
                                    </div>                    
-                            </form>           
+                            </form>  -->         
                  </div>
         </div>
     </div>
